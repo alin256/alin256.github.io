c3274ddd90c2b1bd2e9091422c4d97a9c2c09650
Sergey Alyaev <cobxo3bot@gmail.com>
2017-06-23 22:38:42 +0200

    btnGoogle implementation

--- auth/app.js
+++ auth/app.js
@@ -16,6 +16,7 @@
   const btnLogin = document.getElementById('btnLogin');
   const btnSignUp = document.getElementById('btnSignUp');
   const btnLogout = document.getElementById('btnLogout');
+  const btnGoogle = document.getElementById('btnGoogle');
   
   btnLogin.addEventListener('click', e => {
     //get email and pwd
@@ -39,6 +40,29 @@
 
   btnLogout.addEventListener('click', e => {
     firebase.auth().signOut();
+  });
+
+
+  btnGoogle.addEventListener('click', e => {
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
   });
 
   //Add auth listener
