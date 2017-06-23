780987c5e842800911f1e57fd23b5c05ded95b0b
Dr Sergey Alyaev <cobxo3bot@gmail.com>
2017-06-23 23:47:11 +0200

    added github

--- auth/app.js
+++ auth/app.js
@@ -22,6 +22,7 @@
   const btnSignUp = document.getElementById('btnSignUp');
   const btnLogout = document.getElementById('btnLogout');
   const btnGoogle = document.getElementById('btnGoogle');
+  const btnGitHub = document.getElementById('btnGitHub');
   
   btnLogin.addEventListener('click', e => {
     //get email and pwd
@@ -47,6 +48,28 @@
     firebase.auth().signOut();
   });
 
+  btnGitHub.addEventListener('click', e => {
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = result.credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+      }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  });
 
   btnGoogle.addEventListener('click', e => {
     var provider = new firebase.auth.GoogleAuthProvider();
